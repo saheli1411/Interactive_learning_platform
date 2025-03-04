a29ae7513ed1efab63a9f5421a00153e845a5d72
Saheli Roy <aarushiroy13@gmail.com>
2025-03-04 20:16:35 +0530

    Initial commit: Django setup with PostgreSQL

---
+++ django_app/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learning_platform',
+        'USER': 'your_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
